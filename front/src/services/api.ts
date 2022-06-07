450e548151f4199c705436e3cb13fbf4738ccb42
Geovane Silva <geovanesilva.nar@hotmail.com>
2022-06-07 00:04:18 -0300

    .Ts

--- front/src/services/api.ts
+++ front/src/services/api.ts
@@ -4,6 +4,6 @@ export const api = axios.create({
     baseURL: 'http://localhost:4000',
 });
 
-export const loginApi = async (email, senha) => {
+export const loginApi = async (email: string, senha: string) => {
     return api.post('/login', { email, senha });
 };
